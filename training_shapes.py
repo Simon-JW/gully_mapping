2c1e00cf7ba1e1db628bd8e6f6708b650529fb29
Simon Walker <simon.walker@anu.edu.au>
2017-09-18 20:30:09 +1000

    first commit.

---
+++ training_shapes.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Simon Walker
+#
+# Created:     14/09/2017
+# Copyright:   (c) Simon Walker 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#Write script to create shapes of landscape classes and use those to generate signature files on the 456 image.
