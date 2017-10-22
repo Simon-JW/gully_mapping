f57a910c0b941cde60cd77af3644723ca37e9ceb
Simon Walker <simon.walker@anu.edu.au>
2017-10-23 09:27:50 +1100

    First commit.

---
+++ gully_edge.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Simon Walker
+#
+# Created:     22/10/2017
+# Copyright:   (c) Simon Walker 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
