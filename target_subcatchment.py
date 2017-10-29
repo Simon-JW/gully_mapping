4ef781ab485315448317778b3bbeaf8441706125
Simon Walker <walkers@fses.anu.edu.au>
2017-10-30 10:59:40 +1100

    Added script inputs and outputs to top and tidied up naming of outputs.

--- target_subcatchment.py
+++ target_subcatchment.py
@@ -1,12 +1,22 @@
 #-------------------------------------------------------------------------------
 # Name:        Divide a DEM by subcatchment.
-# Purpose:
+# Purpose:      Create clipped DEM and RGB of target area to do further analysis on.
 #
 # Author:      Simon Walker
 #
 # Created:     06/09/2017
 # Copyright:   (c) Simon Walker 2017
 # Licence:     <your licence>
+
+#Take ~30 seconds per sub-catchment.
+#Requires:
+# 1. DEM file.
+# 2. Shapefile of broader area with multiple subcatchments to select from.
+# 3. Landsat or other optical satellite data with at least three bands.
+
+#Creates:
+# 1. Clipped DEM of target subcatchment.
+# 2. Clipped Landsat RGB of target subcatchment.
 #-------------------------------------------------------------------------------
 # Imports
 import arcpy
@@ -14,30 +24,25 @@ import os
 import time
 t0 = time.time()
 ################################################################################
-#Take ~30 seconds per sub-catchment.
-
-#Requires:
-
-#Creates:
-
-################################################################################
 #Set directories.
-drive = 'C'
+drive = 'X'
 root_dir = drive + ":\PhD\junk"; os.chdir(root_dir)
 out_folder = drive + ":\PhD\junk"
 
-################################################################################
 #Set sub-catchments file and corresponding DEM.
-dem_file = 'qldz56.tif'
-DEM = os.path.join(root_dir, dem_file)
-landsat ='LS8_OLI_TIRS_NBAR_P54_GANBAR01-032_090_078_20140726\scene01'
-landsat_files = os.path.join(root_dir, landsat)
+dem_file = 'mary_5m'
 catchments_shape = 'Mary_subcatchments_mgaz56.shp'
-input_catchments = os.path.join(root_dir, catchments_shape)
+landsat ='LS8_OLI_TIRS_NBAR_P54_GANBAR01-032_090_078_20140726\scene01'
 target_basin = 38 #This is the FID value of the subcatchment of interest.
-bas = "bas" #Short for basin. Is the name of the feature layer created by arcpy.MakeFeatureLayer_management below.
 
 ################################################################################
+#Automatically sets paths to files.
+DEM = os.path.join(root_dir, dem_file)
+landsat_files = os.path.join(root_dir, landsat)
+input_catchments = os.path.join(root_dir, catchments_shape)
+bas = "bas" #Short for basin. Is the name of the feature layer created by arcpy.MakeFeatureLayer_management below.
+
+#------------------------------------------------------------------------------#
 #Function for extracting extents of shapes for defining clipping geometry.
 def extents(fc):
     extent = arcpy.Describe(fc).extent
@@ -53,7 +58,7 @@ def extents(fc):
 #w1, s1, e1, n1, wid1, hgt1 = extents(shape1)
 #w2, s2, e2, n2, wid2, hgt2 = extents(shape2)
 
-################################################################################
+#------------------------------------------------------------------------------#
 # Process: Make Feature Layer
 arcpy.MakeFeatureLayer_management(input_catchments, bas, "", "", "FID FID VISIBLE NONE;Shape Shape VISIBLE NONE;Id Id VISIBLE NONE;gridcode gridcode VISIBLE NONE")
 #This is required because SelectByFeature and SelectByAttribute do not work on shape files using arcpy. Hence they need to first be convereted to feature layers.
@@ -62,7 +67,7 @@ fields = [f.name for f in arcpy.ListFields(bas)]#Just tells me what field names 
 print fields
 cursor = arcpy.da.SearchCursor(bas, [fields[0], fields[1], fields[2], fields[3], fields[4]])
 
-################################################################################
+#------------------------------------------------------------------------------#
 #Clip DEM according to specific sub-catchment specified.
 for row in cursor:
     if row[0] == target_basin:
@@ -73,12 +78,12 @@ for row in cursor:
         print area_shape
         left, bottom, right, top, width, height = extents(area_shape)
         print (left, bottom, right, top, width, height)
-        new = os.path.join(out_folder, dem_file[:3] + str(target_basin))
+        clipped_dem = os.path.join(out_folder, dem_file[:3] + '_' + str(target_basin) + '_DEM')
         extent = str(left) + ' ' + str(bottom) + ' ' + str(right) + ' ' + str(top)
-        arcpy.Clip_management(DEM, extent, new, area_shape, "-999", "true", "NO_MAINTAIN_EXTENT")
-        print new
+        arcpy.Clip_management(DEM, extent, clipped_dem, area_shape, "-999", "true", "NO_MAINTAIN_EXTENT")
+        print clipped_dem
 
-################################################################################
+#------------------------------------------------------------------------------#
 #Syntax for extracting extent of raster.
 
         #dem_raster = arcpy.sa.Raster(DEM)
@@ -88,29 +93,33 @@ for row in cursor:
         #top = int(area_shape.extent.YMax); print top # This is the syntax for getting raster extents.
         #bottom = int(area_shape.extent.YMin); print bottom # This is the syntax for getting raster extents.
 
-################################################################################
+#------------------------------------------------------------------------------#
 #Set landsat output file names.
 band_1 = 'B4'
 band_2 = 'B5'
 band_3 = 'B6'
-rgb_inputs = os.path.join(out_folder, dem_file[:3] + '_' + band_1 + '.tif') +';' + os.path.join(out_folder, dem_file[:3] + '_' + band_2 + '.tif') + ';' + os.path.join(out_folder, dem_file[:3] + '_' + band_3 + '.tif')
-rgb_out = dem_file[:3] + '_' + str(band_1[-1:]) + str(band_2[-1:]) + str(band_3[-1:]) + '.tif'
+rgb_inputs = os.path.join(out_folder,
+        dem_file[:3] + str(target_basin) + '_' + band_1 + '.tif') +';' + os.path.join(out_folder,
+        dem_file[:3] + str(target_basin) + '_' + band_2 + '.tif') + ';' + os.path.join(out_folder,
+        dem_file[:3] + str(target_basin) + '_' + band_3 + '.tif')
+
+rgb_out = dem_file[:3] + '_' + str(target_basin) + '_' + str(band_1[-1:]) + str(band_2[-1:]) + str(band_3[-1:]) + '.tif'
 rgb_file = os.path.join(out_folder, rgb_out)
 #landsat_files = r"C:\PhD\junk\LS8_OLI_TIRS_NBAR_P54_GANBAR01-032_090_078_20140726\scene01"
 os.chdir(landsat_files)
 
-################################################################################
+#------------------------------------------------------------------------------#
 #Clip satellite imagery.
 for (dirpath, dirnames, filenames) in os.walk('.'):
     for file in filenames:
         if file.endswith('.tif'):
-            new_rgb = os.path.join(out_folder, dem_file[:3] + file[-7:])
+            new_rgb = os.path.join(out_folder, dem_file[:3] + str(target_basin) + file[-7:])
             extent = str(left) + ' ' + str(bottom) + ' ' + str(right) + ' ' + str(top)
             arcpy.Clip_management(file, extent, new_rgb, area_shape, "-999", "true", "NO_MAINTAIN_EXTENT")
             print new_rgb
 
 arcpy.CompositeBands_management(rgb_inputs, rgb_file)
-################################################################################
+#------------------------------------------------------------------------------#
 #Syntax for extracting extent of raster.
 
             #image_raster = arcpy.sa.Raster(file)
@@ -119,13 +128,13 @@ arcpy.CompositeBands_management(rgb_inputs, rgb_file)
             #top = int(image_raster.extent.YMax)
             #bottom = int(image_raster.extent.YMin)
 
-################################################################################
+#------------------------------------------------------------------------------#
 #Clean up unwanted files.
 root_dir = drive + ":\PhD\junk"; os.chdir(root_dir)
 #Delete clipped Landsat bands used to create 456 image.
 for (dirpath, dirnames, filenames) in os.walk('.'):
     for file in filenames:
-        if file.startswith(dem_file[:3] + '_B'):
+        if file.startswith(dem_file[:3] + str(target_basin) + '_B'):
             print 'this file will be deleted ' + '' + file
             arcpy.Delete_management(file)
 
@@ -133,7 +142,7 @@ for (dirpath, dirnames, filenames) in os.walk('.'):
 #arcpy.Delete_management(DEM)
 arcpy.Delete_management(area_shape)
 
-################################################################################
+#------------------------------------------------------------------------------#
 print ""
 print "Time taken:"
 print "hours: %i, minutes: %i, seconds: %i" %(int((time.time()-t0)/3600), int(((time.time()-t0)%3600)/60), int((time.time()-t0)%60))
