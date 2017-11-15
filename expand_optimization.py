a136bf1fb190007899238714bb4f4f45ff4d614e
Simon Walker <walkers@fses.anu.edu.au>
2017-11-15 17:00:41 +1100

    First commit.

---
+++ expand_optimization.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      walkers
+#
+# Created:     15/11/2017
+# Copyright:   (c) walkers 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#For the test shapes, it might be best only to take shapes > some value to make sure it does a better job.
+
+# 1. Clip gully raster with bin shape.
+
+# 2. give each raster shape a unique value using...
+
+# 3.
+
+import random
+for x in range(10):
+  print random.randint(1,700)
